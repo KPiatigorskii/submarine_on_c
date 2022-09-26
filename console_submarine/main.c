6c4741eea0c77460a818b15d27eb1677eaa40ffe
KPiatigorskii <k.piatigorskii@gmail.com>
2022-09-26 17:38:54 +0300

    add ship structure

--- console_submarine/main.c
+++ console_submarine/main.c
@@ -3,9 +3,10 @@
 #include "ships.h"
 #include <stdlib.h>
 
-void main() {
-    int area[12][12] = {0};
-    generate_ship(area,4);
+
+void generate_ships() {
+    int area[12][12] = { 0 };
+    generate_ship(area, 4);
     printf("after 1st:\n");
     print_area_0(area);
     generate_ship(area, 3);
@@ -35,4 +36,19 @@ void main() {
     generate_ship(area, 1);
     printf("after 10th:\n");
     print_area_0(area);
-};
+    for (int i = 0; i < 10; i++)
+    {
+        printf("ship id:%d\n", ships[i].id);
+        printf("ship health:%d\n", ships[i].health);
+        printf("ship 1st cell: %d,%d\n", ships[i].coords[0][0], ships[i].coords[0][1]);
+        printf("ship 2st cell: %d,%d\n", ships[i].coords[1][0], ships[i].coords[1][1]);
+        printf("ship 3st cell: %d,%d\n", ships[i].coords[2][0], ships[i].coords[2][1]);
+        printf("ship 4st cell: %d,%d\n", ships[i].coords[3][0], ships[i].coords[3][1]);
+        printf("==================================\n");
+    }
+}
+
+
+void main() {
+    generate_ships();
+}
